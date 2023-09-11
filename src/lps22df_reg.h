c84b3f3b959bd8c1f276fd458c6a1c60d8adf54a
Carlo Parata <carlo.parata@st.com>
2023-09-11 14:55:36 +0200

    Update lps22df_reg.h
    Signed-off-by: Carlo Parata <carlo.parata@st.com>

--- src/lps22df_reg.h
+++ src/lps22df_reg.h
@@ -101,35 +101,6 @@ typedef struct {
 
 #endif /* MEMS_SHARED_TYPES */
 
-/**
-  * @}
-  *
-  */
-
-/** @addtogroup  Interfaces_Functions
-  * @brief       This section provide a set of functions used to read and
-  *              write a generic register of the device.
-  *              MANDATORY: return 0 -> no Error.
-  * @{
-  *
-  */
-
-typedef int32_t (*lps22df_write_ptr)(void *, uint8_t, uint8_t *, uint16_t);
-typedef int32_t (*lps22df_read_ptr)(void *, uint8_t, uint8_t *, uint16_t);
-
-typedef struct {
-  /** Component mandatory fields **/
-  lps22df_write_ptr  write_reg;
-  lps22df_read_ptr   read_reg;
-  /** Customizable optional pointer **/
-  void *handle;
-} lps22df_ctx_t;
-
-/**
-  * @}
-  *
-  */
-
 #ifndef MEMS_UCF_SHARED_TYPES
 #define MEMS_UCF_SHARED_TYPES
 
@@ -155,6 +126,35 @@ typedef struct {
   */
 
 #endif /* MEMS_UCF_SHARED_TYPES */
+
+/**
+  * @}
+  *
+  */
+
+/** @addtogroup  LPS22DF_Interfaces_Functions
+  * @brief       This section provide a set of functions used to read and
+  *              write a generic register of the device.
+  *              MANDATORY: return 0 -> no Error.
+  * @{
+  *
+  */
+
+typedef int32_t (*lps22df_write_ptr)(void *, uint8_t, uint8_t *, uint16_t);
+typedef int32_t (*lps22df_read_ptr)(void *, uint8_t, uint8_t *, uint16_t);
+
+typedef struct {
+  /** Component mandatory fields **/
+  lps22df_write_ptr  write_reg;
+  lps22df_read_ptr   read_reg;
+  /** Customizable optional pointer **/
+  void *handle;
+} lps22df_ctx_t;
+
+/**
+  * @}
+  *
+  */
 
 /** @defgroup LPS22DF_Infos
   * @{
